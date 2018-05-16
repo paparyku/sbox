ab189ab747c9ba998329eb6716b43d7f26512579
paparyku <25714741+paparyku@users.noreply.github.com>
2018-05-16 14:38:46 -0600

    updated views for testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Wow
+                    Sbox
                 </div>
 
                 <div class="links">
