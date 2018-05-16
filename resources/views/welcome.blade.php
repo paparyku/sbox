e9e95004f68953ea9ca493a7403d6c6a35f553a2
paparyku <paparyku@users.noreply.github.com>
2018-05-16 14:13:29 -0600

    updated views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Wow
                 </div>
 
                 <div class="links">
